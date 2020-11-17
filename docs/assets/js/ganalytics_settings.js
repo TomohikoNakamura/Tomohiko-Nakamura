61c0966d0c23a9aeaadb98c6b30468724d094905
Tomohiko Nakamura <tomohiko.nakamura.jp@ieee.org>
2020-11-17 15:50:46 +0900

    [add] google analytics

---
+++ docs/assets/js/ganalytics_settings.js
@@ -0,0 +1,7 @@
+// Global site tag (gtag.js) - Google Analytics
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=G-9EPRVMYY0Z"></script>');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-9EPRVMYY0Z');
+
