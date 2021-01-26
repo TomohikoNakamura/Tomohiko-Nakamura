7ce803cce6585d49d86f9b69f4a1eda61ba369bb
Tomohiko Nakamura <tomohiko.nakamura.jp@ieee.org>
2021-01-26 17:57:04 +0900

    [fix]

--- docs/assets/js/ganalytics_settings.js
+++
@@ -1,7 +0,0 @@
-// Global site tag (gtag.js) - Google Analytics
-document.write('<script async src="https://www.googletagmanager.com/gtag/js?id={{ config.google_analytics[0] }}"></script>');
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', '{{ config.google_analytics[0] }}');
-
