894aee56307333f7876662e3c3be19abfd5573df
Tomohiko Nakamura <tomohiko.nakamura.jp@ieee.org>
2021-01-26 16:02:11 +0900

    [update] site renewal

--- docs/assets/js/ganalytics_settings.js
+++ docs/assets/js/ganalytics_settings.js
@@ -1,7 +1,7 @@
 // Global site tag (gtag.js) - Google Analytics
-document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=G-9EPRVMYY0Z"></script>');
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id={{ config.google_analytics[0] }}"></script>');
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-9EPRVMYY0Z');
+gtag('config', '{{ config.google_analytics[0] }}');
 
